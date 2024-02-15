269b6c4f2ad76b29bf5800856c2334ae80113176
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2024-02-15 22:45:10 +0000

    feat: add ML class

---
+++ machine_learning.py
@@ -0,0 +1,47 @@
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+class MachineLearning:
+    def __init__(self, data_to_use, data_to_target, test_size=0.30, random_state=32):
+
+        self.data_to_use = data_to_use
+        self.data_to_target = data_to_target
+        self.test_size = test_size
+        self.random_state = random_state
+
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+
+        self.knn_model = None
+
+        self.mae_train = None
+        self.rmse_train = None
+        self.r2_train = None
+        self.mse_train = None
+
+
+    def split_data(self):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.data_to_use,
+            self.data_to_target,
+            test_size=self.test_size,
+            random_state=self.random_state
+        )
+
+    def fit_knn_classification(self):
+        knn_model = KNeighborsClassifier(n_neighbors=5)
+        #at this point it just saves the data and do no training
+        self.knn_model = knn_model.fit(self.X_train, self.y_train)
+
+    def predict_model(self, model):
+        y_pred_knn = model.predict(self.X_test)
+        return y_pred_knn
+
+    def evaluate_model(self, y_test_pred):
+        self.mae_train = mean_absolute_error(self.y_test, y_test_pred)
+        self.rmse_train = mean_squared_error(self.y_test, y_test_pred, squared=False)
+        self.r2_train = r2_score(self.y_test, y_test_pred)
+        self.mse_train = mean_squared_error(self.y_test, y_test_pred)
